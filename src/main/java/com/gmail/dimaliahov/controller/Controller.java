8efdc6710f637c1871d8dc18b8ebcbc1a77c57a6
LiakhovDmitriy <dimaliahov@gmail.com>
2020-09-28 14:17:23 +0300

    Login getById --> depending on the role, the answer changes
    working version

--- src/main/java/com/gmail/dimaliahov/controller/Controller.java
+++ src/main/java/com/gmail/dimaliahov/controller/Controller.java
@@ -1,7 +1,9 @@
 package com.gmail.dimaliahov.controller;
 
+import com.gmail.dimaliahov.dto.AdminUserDto;
 import com.gmail.dimaliahov.dto.AuthenticationRequestDto;
 import com.gmail.dimaliahov.dto.UserDto;
+import com.gmail.dimaliahov.model.Role;
 import com.gmail.dimaliahov.model.User;
 import com.gmail.dimaliahov.security.jwt.JwtTokenProvider;
 import com.gmail.dimaliahov.sevice.UserService;
@@ -25,9 +27,9 @@ import java.util.Map;
 public class Controller {
 
 	private final UserService userService;
-
 	private final AuthenticationManager authenticationManager;
 	private final JwtTokenProvider jwtTokenProvider;
+
 	@Autowired
 	public Controller (UserService userService, AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider) {
 		this.userService = userService;
@@ -35,28 +37,40 @@ public class Controller {
 		this.jwtTokenProvider = jwtTokenProvider;
 	}
 
-
-
-
 	@GetMapping (value = "{id}")
-	public ResponseEntity<UserDto> getUserById(@PathVariable (name = "id") Long id){
+	public ResponseEntity<Object> getUserById (@PathVariable (name = "id") Long id) {
 		User user = userService.findById(id);
 
-		if(user == null){
+		if (user == null) {
 			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 		}
-
 		UserDto result = UserDto.fromUser(user);
+
+		boolean roleAdmin = false;
+		for (int i = 0; user.getRoles().size() > i; i++) {
+			Role r = user.getRoles().get(i);
+			if(r.getName().equals("ROLE_ADMIN")){
+				roleAdmin = true;
+				break;
+			}
+
+		}
+
+		if (roleAdmin) {
+			AdminUserDto resultAdmin = AdminUserDto.fromUser(user);
+			System.out.println("Test");
+			return new ResponseEntity<>(resultAdmin, HttpStatus.OK);
+		}
 
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 
 	@PostMapping ("login")
-	public ResponseEntity login(@RequestBody AuthenticationRequestDto requestDto) {
-
+	public ResponseEntity login (@RequestBody AuthenticationRequestDto requestDto) {
 		try {
 			String username = requestDto.getUsername();
-			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, requestDto.getPassword()));
+			String password = requestDto.getPassword();
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
 			User user = userService.findByUsername(username);
 
 			if (user == null) {
@@ -69,10 +83,8 @@ public class Controller {
 			response.put("username", username);
 			response.put("token", token);
 			return ResponseEntity.ok(response);
-		} catch (AuthenticationException e) {
+		} catch (AuthenticationException e){
 			throw new BadCredentialsException("Invalid username or password");
 		}
 	}
-
-
 }
